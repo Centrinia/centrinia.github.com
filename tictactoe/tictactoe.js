12871b196a74d69bf7ab25146d7c5a17e600bf64
Josh <centrinia@gmail.com>
2016-10-11 23:09:12 -0400

    --allow-empty-message

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,6 +1,7 @@
-
 'use strict';
-var autoplay = true;
+
+var timeout = 100;
+var isComputer = {'X' : false,'O':true};
 
 function drawBoard(canvas,context,players) {
 	var cellSize = 200;
@@ -57,13 +58,13 @@ function hasWon(positions) {
 		return (positions & mask) == mask;
 	});
 }
+
 function makeMove(row,column,positions,player) {
 	var index = row*3+column;
 	if((((positions['X']|positions['O']) >> index) & 1) != 0) {
-		return false;
+		throw new Error();
 	}
 	positions[player] |= 1 << index;
-	return true;
 }
 
 function popcount(x) {
@@ -84,19 +85,28 @@ function countTrailingZeros(x) {
 	}
 }
 
+function selectRandom(x) {
+	return x[Math.floor(Math.random() * x.length)];
+}
+
 function dontLose(mine,other) {
 	function positionFromIndex(x) {
 		return { 'row' : Math.floor(x/3),'column': x % 3, 'index' : x};
 	}
 	function win(mine,other) {
+		var outs = [];
 		for(var i=0;i<lines.length;i++) {
 			var line = lines[i];
 			if((popcount(mine & line) == 2) && ((other & line) == 0)) {
 				var empty = line ^ (mine & line);
-				return positionFromIndex(countTrailingZeros(empty));
+				outs.push(empty);
 			}
 		}
-		throw new Error();
+		if(outs.length > 0) {
+			return positionFromIndex(countTrailingZeros(selectRandom(outs)));
+		} else {
+			throw new Error();
+		}
 	}
 	// Win
 	try {
@@ -122,78 +132,113 @@ function dontLose(mine,other) {
 		}
 		throw new Error();
 	}
-	// Fork
-	for(var i=0;i<lines.length;i++) {
-		for(var j=i+1;j<lines.length;j++) {
-			try {
-				return fork(mine,other,lines[i],lines[j]);
-			} catch(e) {}
-		}
-	}
-	// Block Fork
-	for(var i=0;i<lines.length;i++) {
-		for(var j=i+1;j<lines.length;j++) {
-			try {
-				fork(other,mine,lines[i],lines[j]);
-
-				var intersection = lines[i] & lines[j];
-				var unionEmpty = (lines[i] | lines[j]) & ~(other|mine);
-				while(unionEmpty) {
-					var mask = 1 << countTrailingZeros(unionEmpty);
-					for(var k=0;k<lines.length;k++) {
-						var line = lines[k];
-						// Playing this position will create a two-in-a-row for me
-						if((popcount((mask | mine) & line) == 2)) {
-
-						// ... and my opponent will have to block.
-							try {
-								var t = win(mine|mask,other);
-								try {
-									var s = win(other | (1 << t['index']),mask|mine);
-									var u = win(other | (1 << t['index']),mask|mine|(1<<s['index']));
-									console.log(s);
-									console.log(u);
-								} catch (f) {
-									return positionFromIndex(countTrailingZeros(mask));
-								}
-							} catch (e ) {
-							}
-						}
-					}
-
-					unionEmpty ^= mask;
-				}
-				// Occupy the intersection.
-				return positionFromIndex(countTrailingZeros(intersection));
-			} catch(e) {
+	function doFork(mine,other) {
+		var outs = [];
+		for(var i=0;i<lines.length;i++) {
+			for(var j=i+1;j<lines.length;j++) {
+				try {
+					outs.push( {
+						'move': fork(mine,other,lines[i],lines[j]),
+						'lines' : [lines[i],lines[j]]
+					});
+				} catch(e) {}
 			}
 		}
+		if(outs.length > 0) {
+			return selectRandom(outs);
+		} else {
+			throw new Error();
+		}
 	}
+	// Fork
+	try {
+		return doFork(mine,other)['move'];
+	} catch (e) {}
+
+	// Block Fork
+	function blockFork(mine,other) {
+		var blockedFork = doFork(other,mine);
+		
+		var intersection = blockedFork['lines'][0] & blockedFork['lines'][1];
+		var unionEmpty = (blockedFork['lines'][0] | blockedFork['lines'][1]) & ~(other|mine);
+		var outs = [];
+		while(unionEmpty) {
+			var mask = 1 << countTrailingZeros(unionEmpty);
+			for(var k=0;k<lines.length;k++) {
+				var line = lines[k];
+				// Playing this position will create a two-in-a-row for me
+				if((popcount((mask | mine) & line) == 2)) {
+					try {
+						var t = win(mine|mask,other);
+						try {
+							var s = win(other | (1 << t['index']),mask|mine);
+							var u = win(other | (1 << t['index']),mask|mine|(1<<s['index']));
+						} catch(f) {
+						outs.push(mask);
+						}			
+					} catch(e) {
+					}
+				}
+			}
+
+			unionEmpty ^= mask;
+		}
+		if(outs.length > 0) {
+			return positionFromIndex(countTrailingZeros(selectRandom(outs)));
+		} else {
+			// Occupy the intersection.
+			return positionFromIndex(countTrailingZeros(intersection));
+		}
+	}
+	try {
+		return blockFork(mine,other);
+	} catch(e) {};
+
 	// Center
 	if(((mine|other) & 0x10) == 0) {
 		return positionFromIndex(4);
 	}
 
-	// Opposite Corner.
 	var cornersAndOpposite = [[0x01,0x100],[0x4,0x40],[0x40,0x4],[0x100,0x01]];
-	for(var i=0;i<cornersAndOpposite.length;i++) {
-		if(((other & cornersAndOpposite[i][0]) != 0) && (((other | mine) & cornersAndOpposite[i][1]) == 0)) {
-			return positionFromIndex(countTrailingZeros(cornersAndOpposite[i][1]));
+	// Opposite Corner.
+	function oppositeCorner(mine,other) {
+		var outs = [];
+		for(var i=0;i<cornersAndOpposite.length;i++) {
+			if(((other & cornersAndOpposite[i][0]) != 0) && (((other | mine) & cornersAndOpposite[i][1]) == 0)) {
+				outs.push(cornersAndOpposite[i][1]);
+			}
+		}
+		if(outs.length>0) {
+			return positionFromIndex(countTrailingZeros(selectRandom(outs)));
+		} else {
+			throw new Error();
+		}
+	}
+	try {
+		return oppositeCorner(mine,other);
+	} catch(e) {}
+
+	function emptyCell(mine,other,cells) {
+		var outs = [];
+		for(var i=0;i<cells.length;i++) {
+			if(((other | mine) & cells[i]) == 0) {
+				outs.push(cells[i]);
+			}
+		}
+		if(outs.length>0) {
+			return positionFromIndex(countTrailingZeros(selectRandom(outs)));
+		} else {
+			throw new Error();
 		}
 	}
 	// Empty Corner.
-	for(var i=0;i<cornersAndOpposite.length;i++) {
-		if(((other | mine) & cornersAndOpposite[i][0]) == 0) {
-			return positionFromIndex(countTrailingZeros(cornersAndOpposite[i][0]));
-		}
-	}
-	// Empty Side
-	var sides = [0x2,0x8,0x20,0x80];
-	for(var i=0;i<sides.length;i++) {
-		if(((other | mine) & sides[i]) == 0) {
-			return positionFromIndex(countTrailingZeros(sides[i]));
-		}
-	}
+	try {
+		return emptyCell(mine,other,[0x1,0x100,0x4,0x40]);
+	} catch(e) {}
+	// Empty Side.
+	try {
+		return emptyCell(mine,other,[0x2,0x8,0x20,0x80]);
+	} catch(e) {}
 }
 
 $(document).ready(function() {
@@ -201,39 +246,79 @@ $(document).ready(function() {
 	var context = canvas.getContext('2d');
 	
 	var positions;
-	var player = ['X','O'];
+	var player;
+	var moves;
+
+	$('.playerMenu > li').bind('mouseover', openSubMenu);
+	$('.playerMenu > li').bind('mouseout', closeSubMenu);
+	$('a.menuItem').bind('click', handleMenuClick);
+
+	function displayPlayer(side) {
+		var sideSelectorPlayer = '[player=\'' + (isComputer[side] ? 'computer' : 'human') + '\']';
+		var sideSelectorSide = '[side=\'' + side + '\']';
+		var sideText = $('a.menuItem' + sideSelectorPlayer + sideSelectorSide).text();
+		$('#' + side.toLowerCase() + 'playerdisp').text(sideText);
+	}
+
+	displayPlayer('X');
+	displayPlayer('O');
+	function handleMenuClick() {
+		var playerComputer;
+		switch($(this).attr('player')) {
+			case 'human':
+				playerComputer = false;
+				break
+			case 'computer':
+				playerComputer = true;
+				break;
+			default:
+				throw new Error();
+				break
+		}
+		var side = $(this).attr('side')
+		isComputer[side] = playerComputer;
+		displayPlayer(side);
+	}
+	function openSubMenu() {
+		$(this).find('ul').css('visibility', 'visible');	
+	}
+
+	function closeSubMenu() {
+		$(this).find('ul').css('visibility', 'hidden');	
+	}	
+
 	function clearBoard() {
 		positions = {'X':0,'O':0};
+		player = ['X','O'];
+		moves = {'X':null,'O':null};
 		drawBoard(canvas,context,positions);
 	}
-	function handleMove(cellRow,cellColumn) {
-		if(makeMove(cellRow,cellColumn,positions,player[0])) {
-			drawBoard(canvas,context,positions);
-			if(hasWon(positions[player[0]])) {
-				alert(player[0] + ' has won!');
-				clearBoard();
-				return;
-			}
-			if((positions['X'] | positions['O'] & 0x1ff) == 0x1ff) {
-				alert('Tie!');
-				clearBoard();
-				return;
-			}
-			if(autoplay) {
-				var move = dontLose(positions[player[1]],positions[player[0]]);
-				makeMove(move['row'],move['column'],positions,player[1]);
-				drawBoard(canvas,context,positions);
-				if(hasWon(positions[player[1]])) {
-					alert(player[1] + ' has won!');
-					clearBoard();
-					return;
-				}
-			} else {
-				player.reverse();
-			}
+	function handleTurn() {
+		if(isComputer[player[0]]) {
+			moves[player[0]] = dontLose(positions[player[0]],positions[player[1]]);
 		}
+
+		if(moves[player[0]]) {
+			try {
+				makeMove(moves[player[0]]['row'],moves[player[0]]['column'],positions,player[0]);
+				drawBoard(canvas,context,positions);
+				if(hasWon(positions[player[0]])) {
+					alert(player[0] + ' has won!');
+					clearBoard();
+				} else if((positions['X'] | positions['O'] & 0x1ff) == 0x1ff) {
+					clearBoard();
+				} else {
+					moves[player[0]] = null;
+					player.reverse();
+				}
+			} catch(e) {}
+		}
+
+		setTimeout(handleTurn,timeout);
 	}
 	clearBoard();
+	handleTurn();
+
 	$('#canvas').click(function(event) {
 		var rows = 3;
 		var columns = 3;
@@ -244,8 +329,12 @@ $(document).ready(function() {
 		var cellColumn = Math.floor(x / cellWidth);
 		var cellRow = Math.floor(y / cellHeight);
 		if(0 <= cellColumn && cellColumn < columns && 0 <= cellRow && cellRow < rows) {
-			handleMove(cellRow,cellColumn);
+			if(!isComputer[player[0]]) {
+				moves[player[0]] = {'row': cellRow,'column': cellColumn};
+			}
 		}
+		event.preventDefault();
 	});
-	//drawBoard(canvas,context,players);
 });
+
+
